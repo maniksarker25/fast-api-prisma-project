3cf95b54c7560f105b818a1d64bfe243244fe49e
manik <maniksarker652@gmail.com>
2026-06-02 13:31:01 +0600

    create api response format

--- app/modules/products/controller.py
+++ app/modules/products/controller.py
@@ -1,3 +1,4 @@
+from app.core.responses.api_response import send_response
 from app.modules.products import service
 from app.modules.products.mapper import to_product_list, to_product_schema
 from app.modules.products.schema import CreateProductSchema, UpdateProductSchema
@@ -5,28 +6,48 @@ from app.modules.products.schema import CreateProductSchema, UpdateProductSchema
 
 async def get_products():
     products = await service.get_all_products()
-    return {
-        "success": True,
-        "data": to_product_list(products),
-        "total": len(products),
-    }
+    return send_response(
+        status_code=200,
+        success=True,
+        message="Products retrieved successfully",
+        data=products,
+    )
 
 
 async def get_product(product_id: str):
     product = await service.get_product_by_id(product_id)
-    return {"success": True, "data": to_product_schema(product)}
+    return send_response(
+        status_code=200,
+        success=True,
+        message="Product retrieved successfully",
+        data=to_product_schema(product)
+    )
 
 
 async def create_product(body: CreateProductSchema):
     product = await service.create_product(body)
-    return {"success": True, "data": to_product_schema(product)}
+    return send_response(
+        status_code=201,
+        success=True,
+        message="Product created successfully",
+        data=to_product_schema(product)
+    )
 
 
 async def update_product(product_id: str, body: UpdateProductSchema):
     product = await service.update_product(product_id, body)
-    return {"success": True, "data": to_product_schema(product)}
+    return send_response(
+        status_code=200,
+        success=True,
+        message="Product updated successfully",
+        data=to_product_schema(product)
+    )
 
 
 async def delete_product(product_id: str):
     await service.delete_product(product_id)
-    return {"success": True, "message": "Product deleted successfully"}
+    return send_response(
+        status_code=200,
+        success=True,
+        message="Product deleted successfully"
+    )
